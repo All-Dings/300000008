af4e371f784aa8d14d8e89e8e642ab3a07ef857e
Michael Holzheu <michael@holzheu.de>
2025-12-24 21:54:21 +0100

    Add dings-gpt.test.bash

---
+++ dings-gpt.test.bash
@@ -0,0 +1,6 @@
+python3 dings-gpt.py import text file -start-id 300030100 -about-file dings-gpt.about dings-gpt.py
+exit
+python3 dings-gpt.py import text file -start-id 7 -about-file xxx.about -alias dings-gpt dings-gpt.py
+exit
+python3 dings-gpt.py import text file -start-id 7 -about-file xxx.about  010-Plot-B-vs-A.py
+python3 dings-gpt.py  import text file -start-id 7 -about "[CRAT](60106)=[GPT](9000150)" -about 60106=9000150 010-Plot-B-vs-A.py
